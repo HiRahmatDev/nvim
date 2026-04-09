3925a4795d61967f1b815fb15bed2f181a986d36
Rahmat Hidayatullah <hirahmat.dev@gmail.com>
2026-04-09 20:10:33 +0700

    Enable linebreak reorganize options

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -6,15 +6,13 @@
 -- To see documentation for an option, you can use `:h 'optionname'`, for example `:h 'number'`
 -- (Note the single quotes)
 
+-- Basic options
 vim.o.number = true -- Show line numbers in a column.
+vim.o.relativenumber = true -- Show line numbers relative to where the cursor is.
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
-
--- Show line numbers relative to where the cursor is.
--- Affects the 'number' option above, see `:h number_relativenumber`.
-vim.o.relativenumber = true
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.o.ignorecase = true
@@ -22,3 +20,5 @@ vim.o.smartcase = true
 
 vim.o.cursorline = true -- Highlight the line where the cursor is on.
 vim.o.scrolloff = 8 -- Keep this many screen lines above/below the cursor.
+vim.o.wrap = false -- Default to false, you can toggle it by ';z'. See 'core.keymaps:31'.
+vim.o.linebreak = true -- Wrap whole word.
