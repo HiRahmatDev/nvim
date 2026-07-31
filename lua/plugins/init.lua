6af44d070a771dbeda8f449f867823122fe377ec
Rahmat Hidayatullah <hirahmat.dev@gmail.com>
2026-07-31 10:27:34 +0700

    Add more plugins and refactor
    - Diffview
    - mini.pairs and mini.surround
    - nvim-ts-autotag

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,9 @@
 vim.cmd("packadd! nohlsearch")
 
 vim.pack.add({
+	-- Treesitter
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+	{ src = "https://github.com/windwp/nvim-ts-autotag" },
 
 	-- File explorer
 	{ src = "https://github.com/stevearc/oil.nvim" },
@@ -20,6 +22,8 @@ vim.pack.add({
 
 	-- Version control
 	{ src = "https://github.com/neogitorg/neogit" },
+	-- optional deps
+	{ src = "https://github.com/sindrets/diffview.nvim" }, -- no setup required
 	{ src = "https://github.com/lewis6991/gitsigns.nvim" }, -- no setup required
 
 	-- Mason and LSP
@@ -52,9 +56,12 @@ vim.pack.add({
 	{ src = "https://github.com/dstein64/vim-startuptime" }, -- no setup required
 	{ src = "https://github.com/m00qek/baleia.nvim" }, -- no setup required
 	{ src = "https://github.com/nvim-tree/nvim-web-devicons" }, -- no setup required
+	{ src = "https://github.com/nvim-mini/mini.pairs" },
+	{ src = "https://github.com/nvim-mini/mini.surround" },
 })
 
 require("plugins.nvim-treesitter")
+require("nvim-ts-autotag").setup()
 require("plugins.oil")
 require("plugins.neo-tree")
 require("plugins.telescope")
@@ -65,5 +72,7 @@ require("plugins.colorscheme")
 require("plugins.auto-session")
 require("plugins.bufferline")
 require("plugins.lualine")
-require("plugins.ibl")
-require("plugins.quicker")
+require("ibl").setup()
+require("quicker").setup()
+require("mini.pairs").setup()
+require("mini.surround").setup()
--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,12 +1,14 @@
-local neogit = require('neogit')
+local neogit = require("neogit")
 
 neogit.setup({
-   graph_style = 'kitty',
-   commit_editor = {
-      kind = 'tab',
-      staged_diff_split_kind = 'auto'
-   }
+	graph_style = "kitty",
+	commit_editor = {
+		kind = "tab",
+		staged_diff_split_kind = "auto",
+	},
 })
 
-local map = require('utils.keymap')
-map('n', '<leader>gg', neogit.open, 'Open Neogit status')
+local map = require("utils.keymap")
+map("n", "<leader>gg", function()
+	neogit.open({ kind = "floating" })
+end, "Open Neogit status")
